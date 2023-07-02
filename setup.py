2413e6633178f2ea3c55f346a6738485876fdbe9
Rushikesh Suryawanshi <rushikeshsuryawanshi007@gmail.com>
2023-07-02 10:09:27 +0530

    first

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'flask',
+        # Add any other dependencies required for your project
+    ],
+)
